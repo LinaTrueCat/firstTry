7c5ac6b8b7e6f6006f45c9a5d105ed9fe4eb036b
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-03-02 14:33:09 +0200

    Revert "added code in different files"
    This reverts commit cca96a7eee3af9f28cd1305268bea6a7f58ea61a.

--- code/code/main.cpp
+++ code/code/main.cpp
@@ -1,19 +1,5 @@
+#include <vector>
 #include <iostream>
-#include "account.h"
+#include <iomanip>
+
 using namespace std;
-
-int main() {
-    Npc npc1("John", 100, 50);
-    Npc npc2("Alice", 120, 75);
-
-    npc1.talk("Hello!");
-    npc2.talk("Hi there!");
-
-    npc1.showStatus();
-    npc2.showStatus();
-
-    npc1.takeDamage(30);
-    npc1.showStatus();
-
-    return 0;
-}
