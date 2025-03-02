cca96a7eee3af9f28cd1305268bea6a7f58ea61a
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-03-02 14:22:17 +0200

    added code in different files

--- code/code/account.h
+++ code/code/account.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+using namespace std;
+
+class Npc {
+private:
+    string name;
+    int health;
+    int xp;
+
+public:
+    Npc(string n, int h, int x);
+    void talk(string textToSay);
+    bool isAlive();
+    void takeDamage(int damage);
+    void showStatus();
+};
+
+#endif // ACCOUNT_H
--- code/code/main.cpp
+++ code/code/main.cpp
@@ -1,5 +1,19 @@
-#include <vector>
 #include <iostream>
-#include <iomanip>
-
+#include "account.h"
 using namespace std;
+
+int main() {
+    Npc npc1("John", 100, 50);
+    Npc npc2("Alice", 120, 75);
+
+    npc1.talk("Hello!");
+    npc2.talk("Hi there!");
+
+    npc1.showStatus();
+    npc2.showStatus();
+
+    npc1.takeDamage(30);
+    npc1.showStatus();
+
+    return 0;
+}
